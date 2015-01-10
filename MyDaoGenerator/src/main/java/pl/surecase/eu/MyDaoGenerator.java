e4d3679d927b78c69b50e957577151131a4fa54d
Samidh Talsania <samidhtalsania@gmail.com>
2015-01-10 15:27:02 +0530

    GreenDao files auto generated using MyDaoGenerator

---
+++ MyDaoGenerator/src/main/java/pl/surecase/eu/MyDaoGenerator.java
@@ -0,0 +1,20 @@
+package pl.surecase.eu;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class MyDaoGenerator {
+
+    public static void main(String args[]) throws Exception {
+        Schema schema = new Schema(3, "greendao");
+        Entity box = schema.addEntity("Match");
+        box.addIdProperty();
+        box.addStringProperty("t1");
+        box.addStringProperty("t2");
+        box.addStringProperty("t1c");
+        box.addStringProperty("t2c");
+        box.addStringProperty("ETA");
+        new DaoGenerator().generateAll(schema, args[0]);
+    }
+}
